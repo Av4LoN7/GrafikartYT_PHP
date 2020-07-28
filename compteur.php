f5192ba47231491901ed3e4dc302ace6c393abbb
Av4LoN7 <georges.garnier1@gmail.com>
2020-07-28 10:56:46 +0200

    dashboard & compteur rework

--- compteur.php
+++ compteur.php
@@ -1,5 +1,6 @@
 <?php
 define('STORE_COUNTER_ARRAY', glob('compteurs'.DIRECTORY_SEPARATOR.'compteur-?*.txt'));
+define('DATA_PROCESSED', processData());
 define('MONTH', [
     "janvier",
     "fevrier",
@@ -15,28 +16,37 @@ define('MONTH', [
     "decembre"
 ]);
 
-function getAllViewsNumber() : int
-{
-    $tempNumber = 0;
-    foreach (STORE_COUNTER_ARRAY as $filePath) {
-        $tempNumber += (int) file_get_contents($filePath);
-    }
-        return $tempNumber;
-}
-
 function getViewsByYear() : array
 {
-    $byYearViews = [];
-    $processedData = processData();
-    foreach ($processedData as $key => $data) {
-        $byYearViews[$key] = $data["total"];
+    $viewsByYear = [];
+    $tempNumber = null;
+    foreach (DATA_PROCESSED as $key => $data) {
+        $viewsByYear[$key] = $data["total"];
+        $tempNumber += $data["total"];
     }
-    return $byYearViews;
+    array_push($viewsByYear, $tempNumber); // total of views
+    return $viewsByYear;
+}
+
+function getViewsByMonth(int $year) : string
+{
+    $result = null;
+    if(array_key_exists($year, DATA_PROCESSED)) {
+        foreach (DATA_PROCESSED[$year] as $k1 => $item) {
+            if(is_array($item)) {
+                foreach ($item as $k2 => $value) {
+                    $result .= " le " . $k2 . " " . ucfirst(MONTH[$k1]) . " : " . $value . " de vues <br>";
+                }
+            }
+        }
+    }
+    return $result;
 }
 
 //create and return an array structured like this :
-// [year]["total"]
-//       [month][day] => (value) for easier process purpose
+// [year]["total"] => value
+// [year][month][day] => value
+// for easier process
 function processData() : array
 {
     $counterData = [];
@@ -44,40 +54,15 @@ function processData() : array
         $tempProcess = explode("-", $filePath);
         $year = (int)$tempProcess[1];
         $month = (int)$tempProcess[2];
-        $monthFinal = MONTH[$month - 1]; // the string value of the month
         $day = (int) substr($tempProcess[3], 0, -4); // remove ".txt" end file
         $value = (int) file_get_contents($filePath);
 
         if(array_key_exists($year, $counterData)) {
             $counterData[$year]["total"] += $value;
-            if(array_key_exists($month, $counterData[$year])) {
-                $counterData[$year][$monthFinal][$day][] = $value;
-            } else {
-                $counterData[$year][$monthFinal][$day] = $value;
-            }
         } else {
             $counterData[$year]["total"] = $value;
-            $counterData[$year][$monthFinal][$day] = $value;
         }
+        $counterData[$year][$month - 1][$day] = $value;
     }
     return $counterData;
-}
-
-function getViewsByMonth(int $year)
-{
-    $tempArray = processData();
-    $temp2 = [];
-    $result = null;
-    if(array_key_exists($year, $tempArray)) {
-        unset($tempArray[$year]["total"]);
-        $temp2[] = $tempArray[$year];
-        foreach ($temp2 as $k => $v) {
-                foreach ($v as $k1 => $item) {
-                    foreach ($item as $k2 => $value) {
-                        $result .= " le " . $k2 . " " . ucfirst($k1) . " : " . $value . " de vues <br>";
-                    }
-                }
-        }
-    }
-    return $result;
 }
--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,10 @@
 require 'compteur.php';
 // afficher le nombre de vue total // ok
 // afficher le nombre de vue du site par année  ok
-// lorsque l'on clique sur une année (icone) la liste des mois ou le site fut visité s'affiche en dessous ok
-// afficher le nombre de visite pour le mois sélectionné ok
-$nombreDeVuesTotal = getAllViewsNumber();
+// lorsque l'on clique sur une année (icone) la liste des mois où le site fut visité s'affiche en dessous ok
+// afficher le nombre de visite par jour pour chaque mois enregistré ok
 $listViews = getViewsByYear();
+$nombreDeVuesTotal = array_pop($listViews);
 ?>
 <!doctype html>
 <html lang="fr">
@@ -23,7 +23,6 @@ $listViews = getViewsByYear();
     <title>Dashboard</title>
 </head>
 <body>
-
 <div class="container">
     <h1>DashBoard</h1>
     <div class="jumbotron">
@@ -33,8 +32,7 @@ $listViews = getViewsByYear();
         <nav>
             <ul class="list-unstyled">
                 <?php foreach ($listViews as $k => $views): ?>
-                    <li>
-                        <?= $k; ?> : <?= $views; ?> vues sur l'année
+                    <li> <?= $k; ?> : <?= $views; ?> vues sur l'année
                         <a href="#subMenu<?=$k;?>" aria-expanded="false" class="dropdown-toggle" data-toggle="collapse"></a>
                         <ul id="subMenu<?=$k;?>" class="collapse list-unstyled">
                             <li><?= getViewsByMonth($k); ?></li>
@@ -45,8 +43,6 @@ $listViews = getViewsByYear();
         </nav>
     </div>
 </div>
-
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <!-- Popper.JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
